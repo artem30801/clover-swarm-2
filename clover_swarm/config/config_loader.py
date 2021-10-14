564f96e015380f4ebfdcec77e693560a23955dcd
artem30801 <artem30801@gmail.com>
2021-10-14 20:31:06 +0300

    Added xml (launch) files loading to config and some option types

--- clover_swarm/config/config_loader.py
+++ clover_swarm/config/config_loader.py
@@ -4,29 +4,40 @@ import cattr
 from cattr.gen import make_dict_unstructure_fn, make_dict_structure_fn, override
 from cattr.preconf.pyyaml import make_converter
 
+import math
 import enum
 from typing import List, Dict, Optional, Any, Union
+from distutils.util import strtobool
+
+value_factory = attr.Factory(lambda self: self.default, takes_self=True)
 
 
-@attr.define(kw_only=True)
+def converter(attribute):
+    def decorator(func):
+        attribute.converter = func
+        return func
+    return decorator
+
+
+@attr.define(kw_only=True, on_setattr=[attr.setters.convert, attr.setters.validate])
 class ConfigOption:
     # config = attr.field(init=False, default=None)
     name: str = attr.field(init=False, default=None)
-    tree_path: List[str] = attr.field(default=attr.Factory(lambda self: [self.name], takes_self=True))
-    type: str = attr.field()
+    tree_path: List = attr.field(default=attr.Factory(lambda self: [self.name], takes_self=True))
+    # type: str = attr.field()
     default: Optional[Any] = attr.field(default=None)
     description: Optional[str] = attr.field(default=None)
 
-    value: Any = attr.field(init=False, default=attr.Factory(lambda self: self.default, takes_self=True))
+    value: Any = attr.field(init=False, default=value_factory)
 
     @value.validator
     def validate(self, attribute, value):
         if value is None:
             return
 
-        if self.type == "bool":
-            if not isinstance(value, bool):
-                raise ValueError(f"NOT bool {value} {attribute}")
+        # if self.type == "bool":
+        #     if not isinstance(value, bool):
+        #         raise ValueError(f"NOT bool {value} {attribute}")
 
     def set(self, new_value):
         self.value = new_value
@@ -35,13 +46,60 @@ class ConfigOption:
         return self.value
 
 
+@attr.define(kw_only=True, on_setattr=[attr.setters.convert, attr.setters.validate])
+class BoolConfigOption(ConfigOption):
+    default: bool = attr.field(default=False)
+    value: bool = attr.field(init=False, default=value_factory,
+                             validator=attr.validators.instance_of(bool))
+
+    @staticmethod
+    @converter(value)
+    def convert(value):
+        if isinstance(value, str):
+            value = bool(strtobool(value))
+        return value
+
+    # @value.validator
+    # def validate(self, attribute, value):
+    #     attr.validators.instance_of(str)(self, attribute, value)
+
+
+@attr.define(kw_only=True, on_setattr=[attr.setters.convert, attr.setters.validate])
+class NumericConfigOption(ConfigOption):
+    allow_nan: bool = attr.field(default=True)
+    lt: Optional[float] = attr.field(default=None)
+    gt: Optional[float] = attr.field(default=None)
+    lte: Optional[float] = attr.field(default=None)
+    gte: Optional[float] = attr.field(default=None)
+
+    default: Optional[float] = attr.field(default=float("nan"))
+    value: float = attr.field(init=False, default=value_factory)
+
+    @value.validator
+    def validate(self, attribute, value):  # todo clear error messages
+        if not self.allow_nan and math.isnan(value):
+            raise ValueError
+
+        if self.lt is not None and value >= self.lt:
+            raise ValueError
+
+        if self.gt is not None and value <= self.gt:
+            raise ValueError
+
+        if self.lte is not None and value > self.lte:
+            raise ValueError
+
+        if self.gte is not None and value < self.gte:
+            raise ValueError
+
+
 @attr.define(kw_only=True)
 class EnumItem:
     name: str = attr.field(default=None)
     description: Optional[str] = attr.field(default=None)
 
 
-@attr.define(kw_only=True)
+@attr.define(kw_only=True, on_setattr=[attr.setters.convert, attr.setters.validate])
 class ConfigOptionEnum(ConfigOption):
     items: Dict[str, EnumItem] = attr.field(factory=dict)
     strict: bool = attr.field(default=True)
@@ -51,7 +109,7 @@ class ConfigOptionEnum(ConfigOption):
             item.name = key
 
 
-CONFIG_OPTIONS = Union[ConfigOption, ConfigOptionEnum]
+OPTION_TYPES = Union[ConfigOption, BoolConfigOption, ConfigOptionEnum]
 
 
 class ConfigTypes(enum.Enum):
@@ -67,13 +125,42 @@ class Config:
     type: ConfigTypes = attr.field()
     name: str = attr.field()
     description: Optional[str] = attr.field()
-    options: Dict[str, CONFIG_OPTIONS] = attr.field(factory=dict)
+    options: Dict[str, OPTION_TYPES] = attr.field(factory=dict)
 
     def __attrs_post_init__(self):
         for key, option in self.options.items():
             option.name = key
             # option.config = self
 
+    def load_config(self, filename):
+        ...
+
+
+@attr.define(kw_only=True)
+class XMLConfig(Config):
+    def load_config(self, filename):
+        import xml.etree.ElementTree as ET
+
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        for option in self.options.values():
+            tree_path = option.tree_path[0]
+            node = root.find(tree_path)
+            
+            value_action = option.tree_path[1]
+            if value_action == "attrib":
+                attrib_name = option.tree_path[2]
+                value = node.get(attrib_name)
+            elif value_action == "text":
+                value = node.text
+            else:
+                raise ValueError
+
+            option.set(value)
+
+
+CONFIG_TYPES = Union[Config, XMLConfig]
 
 if __name__ == '__main__':
     import os
@@ -88,6 +175,7 @@ if __name__ == '__main__':
 
     def option_factory(o, cls):
         options_mapping = {
+            "bool": BoolConfigOption,
             "enum": ConfigOptionEnum,
         }
 
@@ -96,12 +184,25 @@ if __name__ == '__main__':
         obj = converter.structure(o, option_type)
         return obj
 
-    converter.register_structure_hook(CONFIG_OPTIONS, option_factory)
+    def config_factory(o, cls):
+        configs_mapping = {
+            "xml": XMLConfig
+        }
 
-    config = converter.structure(aruco_descriptor, Config)
+        config_type = configs_mapping.get(o["type"])
+        obj = converter.structure(o, config_type)
+        return obj
+
+    converter.register_structure_hook(OPTION_TYPES, option_factory)
+    converter.register_structure_hook(CONFIG_TYPES, config_factory)
+
+    config = converter.structure(aruco_descriptor, CONFIG_TYPES)
 
     print(config)
+
+    config.load_config("configs/aruco.launch")
+
     config.options["aruco_detect"].set(True)
     print(config.options["aruco_detect"].value)
-    config.options["aruco_detect"].set("h")
+    # config.options["aruco_detect"].set("h")
 
