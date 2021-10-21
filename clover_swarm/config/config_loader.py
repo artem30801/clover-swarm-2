eeee4ea825ec67a3e23f9ddbd36ff0a296910eef
Artem <artem30801@gmail.com>
2021-10-21 16:18:37 +0300

    Added ConfigCollection class for organized config descriptors loading

--- clover_swarm/config/config_loader.py
+++ clover_swarm/config/config_loader.py
@@ -21,7 +21,7 @@ def converter(attribute):
 
 
 @attr.define(kw_only=True, on_setattr=[attr.setters.convert, attr.setters.validate])
-class ConfigOption:
+class Option:
     # config: "Config" = attr.field(init=False, default=None)
     # _export_ref: Optional[Any] = attr.field(init=False)
 
@@ -45,7 +45,7 @@ class ConfigOption:
 
 
 @attr.define(kw_only=True, on_setattr=[attr.setters.convert, attr.setters.validate])
-class BoolConfigOption(ConfigOption):
+class BoolOption(Option):
     default: bool = attr.field(default=False)
     value: bool = attr.field(init=False, default=value_factory,
                              validator=attr.validators.instance_of(bool))
@@ -60,7 +60,7 @@ class BoolConfigOption(ConfigOption):
 
 # fixme: Raises unclear errors
 @attr.define(kw_only=True, on_setattr=[attr.setters.convert, attr.setters.validate])
-class StrConfigOption(ConfigOption):
+class StrOption(Option):
     default: str = attr.field(default=False)
     value: str = attr.field(init=False, default=value_factory,
                             validator=attr.validators.instance_of(str))
@@ -76,7 +76,7 @@ class StrConfigOption(ConfigOption):
 
 
 @attr.define(kw_only=True, on_setattr=[attr.setters.convert, attr.setters.validate])
-class NumericConfigOption(ConfigOption):
+class NumericOption(Option):
     option_type = float
     allow_nan: bool = attr.field(default=False)  # I think, it's good idea to prohibit nan by default
     lt: Optional[option_type] = attr.field(default=None)
@@ -106,7 +106,7 @@ class NumericConfigOption(ConfigOption):
 
 
 @attr.define(kw_only=True, on_setattr=[attr.setters.convert, attr.setters.validate])
-class IntConfigOption(NumericConfigOption):
+class IntOption(NumericOption):
     option_type = int
     default: Optional[option_type] = attr.field(default=float("nan"))
     value: option_type = attr.field(init=False, default=value_factory, converter=option_type)
@@ -119,7 +119,7 @@ class EnumItem:
 
 
 @attr.define(kw_only=True, on_setattr=[attr.setters.convert, attr.setters.validate])
-class ConfigOptionEnum(ConfigOption):
+class EnumOption(Option):
     items: Dict[str, EnumItem] = attr.field(factory=dict)
     strict: bool = attr.field(default=True)
 
@@ -128,13 +128,12 @@ class ConfigOptionEnum(ConfigOption):
     # @value.validator
     # def validate(self, attribute, value):
 
-
     def __attrs_post_init__(self):
         for key, item in self.items.items():
             item.name = key
 
 
-OPTION_TYPES = Union[ConfigOption, BoolConfigOption, ConfigOptionEnum, StrConfigOption]
+OPTION_TYPES = Union[Option, BoolOption, EnumOption, StrOption, NumericOption, IntOption]
 
 
 class ConfigTypes(enum.Enum):
@@ -152,6 +151,8 @@ class Config:
     description: Optional[str] = attr.field()
     options: Dict[str, OPTION_TYPES] = attr.field(factory=dict)
 
+    converter: cattr.GenConverter = attr.field(factory=make_converter)
+
     def __attrs_post_init__(self):
         for key, option in self.options.items():
             option.name = key
@@ -164,14 +165,26 @@ class Config:
         ...
 
 
+# def bool_to_xml_str(value: bool, cls):
+#     return str(value).lower()
+
+
 @attr.define(kw_only=True)
 class XMLConfig(Config):
     tree: ElementTree = attr.field(init=False)
 
     # def _get_node(self, root):
 
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self._register_hooks()
+
+    def _register_hooks(self):
+        self.converter.register_unstructure_hook(bool, lambda value: str(value).lower())
+
     def import_config(self, fd):
-        self.tree = ElementTree.parse(fd)
+        parser = ElementTree.XMLParser(target=ElementTree.TreeBuilder(insert_comments=True))
+        self.tree = ElementTree.parse(fd, parser=parser)
         root = self.tree.getroot()
 
         for option in self.options.values():
@@ -196,7 +209,7 @@ class XMLConfig(Config):
             tree_path = option.tree_path[0]
             node = root.find(tree_path)
 
-            value = str(option.get())
+            value = str(self.converter.unstructure(option.get()))
             value_action = option.tree_path[1]
             if value_action == "attrib":
                 attrib_name = option.tree_path[2]
@@ -206,72 +219,90 @@ class XMLConfig(Config):
             else:
                 raise ValueError
 
-        self.tree.write(fd)  # todo preserve comments and formatting
+        self.tree.write(fd)  # todo preserve formatting?
 
 
 CONFIG_TYPES = Union[Config, XMLConfig]
 
-if __name__ == '__main__':
-    import os
-    # p = os.path.abspath('descriptions/aruco_launch.yaml')
-    p = os.path.abspath('descriptions/aruco_launch.yaml')
 
-    with open(p) as f:
-        aruco_descriptor = yaml.safe_load(f)
+@attr.define()
+class ConfigCollection:
+    converter: cattr.GenConverter = attr.field(factory=make_converter)
+    configs: Dict[str, "Config"] = attr.field(init=False, factory=dict)
 
-    print(aruco_descriptor)
-
-    converter = make_converter()
-
-    def option_factory(o, cls):
+    def option_factory(self, o, cls):
         options_mapping = {
-            "bool": BoolConfigOption,
-            "enum": ConfigOptionEnum,
-            "float": NumericConfigOption,
-            "int": IntConfigOption,
-            "str": StrConfigOption,
+            "bool": BoolOption,
+            "enum": EnumOption,
+            "float": NumericOption,
+            "int": IntOption,
+            "str": StrOption,
         }
 
-        option_type = options_mapping.get(o["type"], ConfigOption)
+        option_type = options_mapping.get(o["type"], Option)
 
-        obj = converter.structure(o, option_type)
+        obj = self.converter.structure(o, option_type)
         return obj
 
-    def config_factory(o, cls):
+    def config_factory(self, o, cls):
         configs_mapping = {
-            "xml": XMLConfig
+            "xml": XMLConfig,
         }
 
         config_type = configs_mapping.get(o["type"])
-        obj = converter.structure(o, config_type)
+        obj = self.converter.structure(o, config_type)
         return obj
 
-    converter.register_structure_hook(OPTION_TYPES, option_factory)
-    converter.register_structure_hook(CONFIG_TYPES, config_factory)
-
-    def structure(cls):
+    def ignore_no_init(self, cls):
         overrides = {a.name: override(omit=True) for a in attr.fields(cls) if not a.init}
-        return make_dict_structure_fn(cls, converter, **overrides)
+        return make_dict_structure_fn(cls, self.converter, **overrides)
+
+    def __attrs_post_init__(self):
+        self._register_hooks()
+
+    def _register_hooks(self):
+        self.converter.register_structure_hook(OPTION_TYPES, self.option_factory)
+        self.converter.register_structure_hook(CONFIG_TYPES, self.config_factory)
+
+        # Apply to all attr classes
+        self.converter.register_structure_hook_factory(attr.has, self.ignore_no_init)
+
+    def load_descriptor(self, fd, import_config=True):
+        with open(p) as f:
+            descriptor_unstructured = yaml.safe_load(f)
+
+        config = self.converter.structure(descriptor_unstructured, CONFIG_TYPES)
+        self.configs[config.name] = config
+
+        if import_config:
+            config.import_config(config.path)
+
+        return config
+
+    def load_descriptors(self, path):
+        # load folder
+        pass
 
 
-    converter.register_structure_hook_factory(attr.has, structure)
+if __name__ == '__main__':
+    import os
+    p = os.path.abspath('descriptions/aruco_launch.yaml')
+    # p = os.path.abspath('descriptions/led_launch.yaml')
 
-    config = converter.structure(aruco_descriptor, CONFIG_TYPES)
+    collection = ConfigCollection()
+    collection.load_descriptor(p)
 
-    config.import_config(config.path)
+    for config in collection.configs.values():
+        print(config.name)
+        print(config)
 
-    for option in config.options.values():
-        print(option.name, option.value, type(option.value))
+        for option in config.options.values():
+            print(option.name, option.value, type(option.value))
 
-    config.options["length"].set(1)
-    for option in config.options.values():
-        print(option.name, option.value, type(option.value))
+        config.export_config("configs/led_export.launch")
+
+    # config.options["aruco_detect"].set(True)
     # config.options["gpio_pin"].value = "1243"
     # print(config.options["gpio_pin"].value)
 
     # with open("configs/aruco_export.launch", "w") as f:
-    # config.export_config("configs/aruco_export.launch")
-
-
-# config.options["aruco_detect"].set("h")
-
