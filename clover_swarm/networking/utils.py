e3784b56b4c7e1e4e5457074b2cbf0bd0c4ad6d5
Artem <artem30801@gmail.com>
2021-11-02 22:41:20 +0300

    Added utils to retrieve active network interface(s)

--- clover_swarm/networking/utils.py
+++ clover_swarm/networking/utils.py
@@ -4,7 +4,12 @@ from typing import Optional, cast
 import asyncio
 from asyncio import get_running_loop
 
+import anyio
+from anyio.abc import SocketAttribute
 from anyio._backends._asyncio import UDPSocket, DatagramProtocol
+
+import psutil
+import ipaddress
 
 
 async def create_broadcast_socket(
@@ -34,3 +39,71 @@ async def create_broadcast_socket(
         raise protocol.exception
 
     return UDPSocket(transport, protocol)
+
+
+async def get_ip(remote_host="8.8.8.8", remote_port=80):
+    sock = await anyio.create_connected_udp_socket(remote_host=remote_host, remote_port=remote_port)
+    ip = sock.extra(SocketAttribute.local_address)[0]
+    return ip
+
+
+def get_interface_stats():
+    addresses = psutil.net_if_addrs()
+    stats = psutil.net_if_stats()
+
+    interface_stats = {interface: (addrs, stats[interface]) for interface, addrs in addresses.items()}
+    return interface_stats
+
+
+def interface_is_active(interface, interface_stats=None):
+    interface_stats = interface_stats or get_interface_stats()
+    try:
+        addresses, stats = interface_stats[interface]
+    except KeyError:
+        return False
+    
+    if not stats.isup:
+        return False
+
+    return socket.AF_INET in [snicaddr.family for snicaddr in addresses]
+
+
+def get_active_interfaces(interface_stats=None):
+    interface_stats = interface_stats or get_interface_stats()
+    return [interface for interface in interface_stats.keys()
+            if interface_is_active(interface, interface_stats)]
+
+
+def _filter_interfaces(my_ip, interface_stats):
+    for interface, (addresses, _) in interface_stats.items():
+        for snicaddr in addresses:
+            if snicaddr.family == socket.AF_INET and snicaddr.address == my_ip:
+                return snicaddr
+
+
+async def get_active_interface() -> ipaddress.IPv4Interface:
+    my_ip = await get_ip()
+    interface_stats = await anyio.to_thread.run_sync(get_interface_stats)
+
+    # interfaces = get_active_interfaces(interface_stats)
+    snicaddr = _filter_interfaces(my_ip, interface_stats)
+    if snicaddr is None:
+        # return None?
+        raise RuntimeError("No interfaces found")
+
+    return ipaddress.IPv4Interface(f"{snicaddr.address}/{snicaddr.netmask}")
+
+
+if __name__ == '__main__':
+    async def main():
+        print(list(get_interface_stats()))
+        print(get_active_interfaces(), sep="\n")
+        print(await get_ip())
+        interface = await get_active_interface()
+        print(interface)
+        print(interface.network.broadcast_address)
+    # logging.basicConfig(level=logging.DEBUG)
+
+    # asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
+
